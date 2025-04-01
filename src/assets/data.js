e8d975ae9899513fa270dfb3184d1f7ab0dda0e9
Susmita Gorai <132455480+Susmita0202@users.noreply.github.com>
2025-04-01 15:09:16 +0530

    deploy react to github

---
+++ src/assets/data.js
@@ -0,0 +1,123 @@
+  export const data = [
+    {
+      question: "Which is the fastest land animal?",
+      option1: "Cheetah",
+      option2: "Lion",
+      option3: "Horse",
+      option4: "Greyhound",
+      ans: 1
+    },
+    {
+      question: "What is the largest species of shark?",
+      option1: "Great White Shark",
+      option2: "Tiger Shark",
+      option3: "Whale Shark",
+      option4: "Hammerhead Shark",
+      ans: 3
+    },
+    {
+      question: "Which bird is known for its ability to mimic human speech?",
+      option1: "Crow",
+      option2: "Parrot",
+      option3: "Eagle",
+      option4: "Owl",
+      ans: 2
+    },
+    {
+      question: "What is the largest land mammal?",
+      option1: "African Elephant",
+      option2: "Giraffe",
+      option3: "Hippopotamus",
+      option4: "Polar Bear",
+      ans: 1
+    },
+    {
+      question: "Which animal is known as the 'King of the Jungle'?",
+      option1: "Tiger",
+      option2: "Leopard",
+      option3: "Lion",
+      option4: "Panther",
+      ans: 3
+    },
+    {
+      question: "What type of animal is a Komodo dragon?",
+      option1: "Snake",
+      option2: "Lizard",
+      option3: "Crocodile",
+      option4: "Amphibian",
+      ans: 2
+    },
+    {
+      question: "Which mammal is capable of true flight?",
+      option1: "Squirrel",
+      option2: "Bat",
+      option3: "Flying Fox",
+      option4: "Kangaroo",
+      ans: 2
+    },
+    {
+      question: "Which animal is known to have the strongest bite force?",
+      option1: "Lion",
+      option2: "Crocodile",
+      option3: "Shark",
+      option4: "Hyena",
+      ans: 2
+    },
+    {
+      question: "Which bird has the largest wingspan?",
+      option1: "Bald Eagle",
+      option2: "Albatross",
+      option3: "Condor",
+      option4: "Pelican",
+      ans: 2
+    },
+    {
+      question: "What is a group of lions called?",
+      option1: "Pack",
+      option2: "Flock",
+      option3: "Herd",
+      option4: "Pride",
+      ans: 4
+    },
+    {
+      question: "What do pandas primarily eat?",
+      option1: "Grass",
+      option2: "Bamboo",
+      option3: "Fruits",
+      option4: "Small Animals",
+      ans: 2
+    },
+    {
+      question: "How many legs does a spider have?",
+      option1: "4",
+      option2: "6",
+      option3: "8",
+      option4: "10",
+      ans: 3
+    },
+    {
+      question: "Which marine animal is known for having three hearts?",
+      option1: "Dolphin",
+      option2: "Octopus",
+      option3: "Whale",
+      option4: "Jellyfish",
+      ans: 2
+    },
+    {
+      question: "What is the tallest animal in the world?",
+      option1: "Elephant",
+      option2: "Giraffe",
+      option3: "Kangaroo",
+      option4: "Gorilla",
+      ans: 2
+    },
+    {
+      question: "What is the only continent where penguins are found in the wild?",
+      option1: "Europe",
+      option2: "Antarctica",
+      option3: "Asia",
+      option4: "North America",
+      ans: 2
+    }
+  ];
+  
